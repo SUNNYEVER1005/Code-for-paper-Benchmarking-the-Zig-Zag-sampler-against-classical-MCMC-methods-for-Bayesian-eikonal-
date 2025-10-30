c3c8eefcfd628ea5adb250e800143a5dcf909fcc
XXXXXX <143702264+SUNNYEVER1005@users.noreply.github.com>
2025-10-30 23:16:15 +0800

    Update traveltime_continous.m

--- traveltime_continous.m
+++ traveltime_continous.m
@@ -15,33 +15,3 @@ time=zeros(size(T_gridip,1),1);Tx=time;Tz=Tx;  % size(receiver,1)
     value3=Tz;
 
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-% %%  T_m(x_{source},z_{source})
-% time=zeros(size(T,3),1);Tx=time;Tz=Tx;  % size(receiver,1)
-% 
-%     for ii=1:size(time,1)
-%         time(ii)=matrix_interpolate_function(T(:,:,ii), theta(1),theta(2),x_interval,z_interval,dx,dz);
-%         Tx(ii)=matrix_interpolate_function(gradTx(:,:,ii), theta(1),theta(2),x_interval,z_interval,dx,dz);
-%         Tz(ii)=matrix_interpolate_function(gradTz(:,:,ii), theta(1),theta(2),x_interval,z_interval,dx,dz);
-%     end
-%     value1= time ;
-%     value2=Tx;
-%     value3=Tz;
-% 
-% end
